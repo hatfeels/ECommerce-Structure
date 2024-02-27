c651374ae2928ff16b89f1276a37f49763febe45
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2024-02-27 14:45:56 -0300

    done

--- microservice-admin/src/main/java/com/ecommercestructure/admin/MicroserviceAdminApplication.java
+++ microservice-admin/src/main/java/com/ecommercestructure/admin/MicroserviceAdminApplication.java
@@ -2,7 +2,9 @@ package com.ecommercestructure.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceAdminApplication {
 
